421e47a7626d74a26508e9ad7cdbc4da585ee2bb
csabiskar <csabiskar@gmail.com>
2026-03-01 01:09:09 +0530

    Refactor code structure for improved readability and maintainability

---
+++ src/app/font.js
@@ -0,0 +1,14 @@
+import {Poppins,Space_Grotesk} from 'next/font/google'
+
+
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space",
+});
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function Navbar() {
   return (
-    <nav className="bg-black text-white shadow-md">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-        
-        {/* Logo */}
-        <h1 className="text-2xl font-bold tracking-wide cursor-pointer">
-          MyPortfolio
-        </h1>
-
-        {/* Menu */}
-        <ul className="flex gap-8 font-medium">
-          <li className="hover:text-gray-400 cursor-pointer transition duration-300">Home</li>
-          <li className="hover:text-gray-400 cursor-pointer transition duration-300">About</li>
-          <li className="hover:text-gray-400 cursor-pointer transition duration-300">Skills</li>
-          <li className="hover:text-gray-400 cursor-pointer transition duration-300">Certification</li>
-          <li className="hover:text-gray-400 cursor-pointer transition duration-300">Contact Us</li>
-        </ul>
-
+    <>
+      <div className="bg-yellow-300 text-center">
+        Free shipping on orders over 500 ✨.
       </div>
-    </nav>
-  )
+      <nav className="flex h-10 bg-black w-full text-white justify-between">
+        <ul className="flex gap-12 items-center mx-auto">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Certification</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
