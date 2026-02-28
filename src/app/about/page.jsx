a6e47e0cb03f4bc0ccda3e5a2782887d93550ac8
csabiskar <csabiskar.com>
2026-02-28 23:23:08 +0530

    Add Navbar component and About page; remove unused globals.css

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+function page() {
+  return (
+   <>
+   <Navbar/>
+    <h1>about us....</h1>
+   </>
+  )
+}
+
+export default page
