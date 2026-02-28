65e5ffc9633051592bc480f73f64a8d0702984b8
csabiskar <csabiskar.com>
2026-02-28 23:24:46 +0530

    Add Hero component and update page layout title

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-  <>
-    <h1>hello world...</h1>
-     <h1>hello world...</h1>
+    <>
+      <Hero />
       <h1>hello world...</h1>
-       <h1>hello world...</h1>
-        <h1>hello world...</h1>
-  </>
+      <h1>hello world...</h1>
+      <h1>hello world...</h1>
+      <h1>hello world...</h1>
+      <h1>hello world...</h1>
+    </>
   );
 }
